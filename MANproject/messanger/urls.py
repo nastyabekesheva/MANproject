c244c9f5807e100c8bb340e9cfe35ff3fa8c9fc9
nastyabekesheva <40558678+nastyabekesheva@users.noreply.github.com>
2019-11-29 21:52:36 +0200

    Update urls.py

--- MANproject/messanger/urls.py
+++ MANproject/messanger/urls.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from django.urls import include, path
 from .views import SignUpView
 from .views import SignInView
+from .views import StartView
 urlpatterns = [
 	path('sign_up/', SignUpView.as_view(), name='sign_up'),
-	path('sign_in/', SignInView.as_view(), name='sign_in')
+	path('sign_in/', SignInView.as_view(), name='sign_in'),
+	path('start/', StartView.as_view(), name='start')
 			]
