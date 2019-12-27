7955116f0d4148c8e3bfa1529905e6ed71f1462f
nastyabekesheva <40558678+nastyabekesheva@users.noreply.github.com>
2019-12-27 22:54:53 +0200

    Update urls.py

--- MANproject/messanger/urls.py
+++ MANproject/messanger/urls.py
@@ -6,7 +6,7 @@ from .views import StartView
 urlpatterns = [
 	path('sign_up/', SignUpView.as_view(), name='sign_up'),
 	path('sign_in/', SignInView.as_view(), name='sign_in'),
-	path('start/', StartView.as_view(), name='start')
+	path('start/', StartView.as_view(), name='start'),
 	path(r'^chat/$', login_required(views.DialogsView.as_view()), name='chat'),
 	path(r'^chat/create/(?P<user_id>\d+)/$', login_required(views.CreateChatView.as_view()), name='create_chat'),
 	path(r'^chat/(?P<chat_id>\d+)/$', login_required(views.ChatListView.as_view()), name='messages'),
