8c8f6bca8d9099246156b93d38a36bb942c413b9
nastyabekesheva <40558678+nastyabekesheva@users.noreply.github.com>
2019-12-28 19:29:44 +0200

    Update urls.py

--- MANproject/messanger/urls.py
+++ MANproject/messanger/urls.py
@@ -3,11 +3,15 @@ from django.urls import include, path
 from .views import SignUpView
 from .views import SignInView
 from .views import StartView
+from .views import DialogsView
+from .views import CreateChatView
+from .views import ChatListView
+from django.contrib.auth.decorators import login_required
 urlpatterns = [
 	path('sign_up/', SignUpView.as_view(), name='sign_up'),
 	path('sign_in/', SignInView.as_view(), name='sign_in'),
 	path('start/', StartView.as_view(), name='start'),
-	path(r'^chat/$', login_required(views.DialogsView.as_view()), name='chat'),
-	path(r'^chat/create/(?P<user_id>\d+)/$', login_required(views.CreateChatView.as_view()), name='create_chat'),
-	path(r'^chat/(?P<chat_id>\d+)/$', login_required(views.ChatListView.as_view()), name='messages'),
+	path(r'^chat/$', login_required(DialogsView.as_view()), name='chat'),
+	path(r'^chat/create/(?P<user_id>\d+)/$', login_required(CreateChatView.as_view()), name='create_chat'),
+	path(r'^chat/(?P<chat_id>\d+)/$', login_required(ChatListView.as_view()), name='messages'),
 			]
