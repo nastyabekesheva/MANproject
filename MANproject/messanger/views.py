8ae977059de2bbf11a7d9dc3659c34f10609c9b8
nastyabekesheva <40558678+nastyabekesheva@users.noreply.github.com>
2020-01-04 17:26:09 +0200

    Update views.py

--- MANproject/messanger/views.py
+++ MANproject/messanger/views.py
@@ -23,7 +23,7 @@ lass SignUpView(View):
                 raw_password = form.cleaned_data.get('password1')
                 user = authenticate(username=username, password=raw_password)
                 login(request, user)
-                return redirect('/sign_up.html')
+                return redirect('sign_up')
         else:
        	    form = UserCreationForm()
         return render(request, 'messanger/sign_up.html', {'form': form})
